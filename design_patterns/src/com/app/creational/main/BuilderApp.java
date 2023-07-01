a2fa065d91762050fccb99d3218434e6622b9736
Vivek Chaudhary <vkc369258@gmail.com>
2023-07-01 16:36:01 +0530

    Create builder design pattern

--- design_patterns/src/com/app/creational/main/BuilderApp.java
+++ design_patterns/src/com/app/creational/main/BuilderApp.java
@@ -1,7 +1,16 @@
 package com.app.creational.main;
 
+import com.pattern.creational.builder.Apartment;
+import com.pattern.creational.builder.ApartmentBuilder;
+
 public class BuilderApp {
 	public static void main(String[] args) {
-
+		ApartmentBuilder ap = new ApartmentBuilder();
+		
+		Apartment mumbaiHouse = ap.setSqm(300).setRooms(2).setArea("Nala Sopara Sec 04").setCity("Mumbai").build();
+		System.out.println(mumbaiHouse);
+		
+		Apartment miamiHouse = ap.setSqm(6400).setRooms(11).setArea("13C Paradise road, Beverly Hills").setCity("Miami").build();
+		System.out.println(miamiHouse);
 	}
 }
--- design_patterns/src/com/pattern/creational/builder/Apartment.java
+++ design_patterns/src/com/pattern/creational/builder/Apartment.java
@@ -1,5 +1,83 @@
 package com.pattern.creational.builder;
 
 public class Apartment {
-	
+	private int sqm;
+	private int rooms;
+	private String city;
+	private String area;
+	private boolean kitchen;
+
+	public Apartment(int sqm, int rooms, String city, String area, boolean kitchen) {
+		super();
+		this.sqm = sqm;
+		this.rooms = rooms;
+		this.city = city;
+		this.area = area;
+		this.kitchen = kitchen;
+	}
+
+	public int getSqm() {
+		return sqm;
+	}
+
+	public void setSqm(int sqm) {
+		this.sqm = sqm;
+	}
+
+	public int getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(int rooms) {
+		this.rooms = rooms;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public boolean isKitchen() {
+		return kitchen;
+	}
+
+	public void setKitchen(boolean kitchen) {
+		this.kitchen = kitchen;
+	}
+
+	@Override
+	public String toString() {
+		String ap = "Apartment { ";
+
+		if (sqm != -1)
+			ap += " sqm: " + sqm;
+
+		if (rooms != -1)
+			ap += ", rooms: " + rooms;
+
+		if (!city.isEmpty())
+			ap += ", city: " + city;
+
+		if (!area.isEmpty())
+			ap += ", area: " + area;
+
+		if (kitchen || !kitchen)
+			ap += ", kitchen: " + kitchen;
+
+		ap += " }";
+		
+		return ap;
+	}
+
 }
