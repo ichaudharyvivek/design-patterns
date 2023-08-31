71fdb91ef05babaa107c677452b4cd1fe7e3ca2c
Vivek Chaudhary <vkc369258@gmail.com>
2023-08-31 16:55:35 +0000

    Added experimentation feature in AdapterApp.java

--- design_patterns/src/com/app/structural/main/AdapterApp.java
+++ design_patterns/src/com/app/structural/main/AdapterApp.java
@@ -1,7 +1,10 @@
 package com.app.structural.main;
 
+import com.pattern.structural.adapter.AdapterTest;
+
 public class AdapterApp {
 	public static void main(String[] args) {
 		System.out.println("Testing adapter pattern in gitpod.");
+		System.out.println(AdapterTest.test());
 	}
 }
---
+++ design_patterns/src/com/pattern/structural/adapter/AdapterTest.java
@@ -0,0 +1,7 @@
+package com.pattern.structural.adapter;
+
+public class AdapterTest {
+    public static String test() {
+        return "This is a test for adapter pattern";
+    }
+}
